154861324a2e27290cb687c2873854693caf6ba6
vaasut <vaasut1@gmail.com>
2022-10-13 23:16:30 -0400

    Add model visualization code

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild} from '@angular/core';
 import { FormControl } from '@angular/forms';
 import {NgForm} from '@angular/forms';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-features',
@@ -8,21 +11,27 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./features.component.scss']
 })
 export class FeaturesComponent implements OnInit {
+  path = "";
+  show = "none";
 
   mlModels: string[] = ["XG BOOST", "RNN", "Linear Regression", "lstm", "Random Forest", "HMM"]; 
   features: string[] = ["Temperature", "Humidity", "Precipitation"];
   datasets: string[] = ["Prediction target only", "Entire feature set"]
   times: string[] = ["1 day ahead", "1 week ahead", "1 month ahead"]
   dateControl = new FormControl();
+
+ 
   
-  
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
   }
+
    onSubmit(f: NgForm) {
     console.log(f.value);  // { first: '', last: '' }
     console.log(f.valid);  // false
+    this.show = "block";
+    this.path = f.value["datasetField"] + "/" + f.value["timeField"] +  "/" + f.value["features"] + "/" + f.value["mlModels"];
   }
 
 }
