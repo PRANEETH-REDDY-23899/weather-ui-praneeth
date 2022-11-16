e4cf526fdfdf228aaedf3185b0d71bd7197b7b64
Srinivas_Ganta <sganta@albany.edu>
2022-11-16 00:26:24 -0500

    Refactoring Model Components

--- src/app/model/model.component.ts
+++ src/app/model/model.component.ts
@@ -10,6 +10,39 @@ export class ModelComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    document.addEventListener("DOMContentLoaded", function () {
+      document
+        .querySelectorAll(".sidebar .nav-link")
+        .forEach(function (element) {
+          element.addEventListener("click", function (e) {
+            let nextEl = element.nextElementSibling;
+            let parentEl = element.parentElement;
+
+            if (nextEl) {
+              e.preventDefault();
+              // @ts-ignore
+              let mycollapse = new bootstrap.Collapse(nextEl);
+
+              if (nextEl.classList.contains("show")) {
+                mycollapse.hide();
+              } else {
+                mycollapse.show();
+                // find other submenus with class=show
+                var opened_submenu =
+                // @ts-ignore
+                  parentEl.parentElement.querySelector(".submenu.show");
+                // if it exists, then close all of them
+                if (opened_submenu) {
+                  // @ts-ignore
+                  new bootstrap.Collapse(opened_submenu);
+                }
+              }
+            }
+          });
+        });
+    });
   }
+  
 
 }
