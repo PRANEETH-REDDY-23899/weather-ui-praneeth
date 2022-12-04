5653fafba0609ce61a61f184ac89646aca8ab84d
vaasut <vaasut1@gmail.com>
2022-12-03 21:54:29 -0500

    Add other model interpretation text for each model

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -15,9 +15,9 @@ import { Router } from '@angular/router';
 export class FeaturesComponent implements OnInit {
   performancePath = "";
   comparisonPath = "";
-  modelDescription = "Before reading the interpretations, we would ask you to read through the basic understanding of the model in Time Series Machine Learning Models. If you have gone through it, you would know that ARIMA has three hyperparameters – p, q, d and it is important to have the perfect combination of these three hyperparameters so that ARIMA works optimally on the data. In weather data, p and q is assigned the value you choose as window size. On the other hand, d is computed by counting the number of times differencing is done so that the correlation score is reduced below 0.05, i.e., the stationarity is removed completely. This is done by the adfuller function of statsmodel. You can refer to the code snippet of ARIMA to see how these are implemented."
-  modelObservation = "If you look at the results, you will notice that ARIMA doesn’t perform as good as the other models. Temperature and humidity have seasonality and ARIMA doesn’t perform well with seasonal data. On the other hand, precipitation has an interesting result in comparison to temperature and humidity. We observe that precipitation is not seasonal and is quite random. What makes this interesting is that the models don’t perform well either like ARIMA except SVR."
-  modelAnalysis= "A possible reason for this weak performance is seasonality of the trend of temperature and thus the choice of p and q, which in this case would be optimal if chosen as 365, i.e., if ARIMA can peek into the past one year’s information, it would be easier for it to make a proper decision. However, the increase in exponential time complexity of the model with higher p and q values led us to choose smaller values for window which directly reflects in the prediction and the mean absolute score. Another important aspect is that ARIMA heavily depends on the past information and due to the data’s highly changing nature, it is important that there is a large amount of data. In this case, we don’t have enough data In conclusion, in recent years, other machine learning techniques (e.g., Linear Regression, Random Forest, Support vector machines) and other deep learning techniques (e.g., RNN and LSTM) have replaced these classical machine learning models due to their reduced complexity and reduced need for larger dataset for similar performance. This can be seen in the mean absolute square error comparison with other models especially for humidity and temperature."
+  modelDescription = "";
+  modelObservation = "";
+  modelAnalysis= "";
   performanceShow = "none";
   comparisonShow = "none";
 
@@ -44,9 +44,9 @@ export class FeaturesComponent implements OnInit {
       let pathDataset = "prediction_with_target_only";
       this.comparisonShow = "block";
       if (f.value["mlModels"] == "Auto_Regression"){
-      this.modelDescription = "";
-      this.modelObservation = "";
-      this.modelAnalysis = "";
+      this.modelDescription = "Before reading the interpretations, we would ask you to read through the basic understanding of the model in Time Series Machine Learning Models. If you have gone through it, you would know that AR has one hyperparameter – p which is the number of lagged data the model investigates to make a prediction. In weather data, p is assigned the value you choose as window size. You can refer to the code snippet of AR to see how these are implemented.";
+      this.modelObservation = "If you look at the results, you will notice that AR performs better than other classical models at predicting temperature, worse at predicting humidity, and almost similar to the other classical models at predicting precipitation. The model is also insensitive to the window size";
+      this.modelAnalysis = "A possible reason for insensitivity to window size is that the optimal window size would be 365, i.e., if AR can peek into the past one year’s information, it would be easier for it to make a proper decision. However, the increase in exponential time complexity of the model with higher p values led us to choose smaller values for window which directly reflects in the prediction and the mean absolute score. AR performs relatively better with temperature due to its straightforward trend and reduced fluctuation. Whereas higher fluctuation causes AR to perform poorly at predicting humidity. For precipitation, the model just doesn’t get enough data to predict the highly fluctuating variable.";
       this.performancePath = "timeseriesModels/" + f.value["mlModels"] + "/performanceCharts/" + "CHES_AR_" + pathDataset + "_" + f.value["features"] + "_" + f.value["windowSize"];
       this.comparisonPath =  "timeseriesModels/" + f.value["mlModels"] + "/comparisonCharts/" + "CHES_AR_" + pathDataset + "_" + f.value["features"] + "_" + f.value["windowSize"];
       }
--- src/app/featuresSupervised/featuresSupervised.component.ts
+++ src/app/featuresSupervised/featuresSupervised.component.ts
@@ -15,6 +15,9 @@ export class FeaturesSupervisedComponent implements OnInit {
   performancePath = "";
   secondPerformancePath="";
   comparisonPath = "";
+  modelDescription = "";
+  modelObservation = "";
+  modelAnalysis= "";
   performanceShow = "none";
   comparisonShow = "none";
 
@@ -53,12 +56,21 @@ export class FeaturesSupervisedComponent implements OnInit {
 
       if (f.value["mlModels"][0] == "M"){
         modelAbbreviation = "LR";
+          this.modelDescription = "Multiple linear regression (MLR) uses multiple dependent variables to predict the outcome of an independent variable by modelling the linear relationship between the dependent and independent variables.";
+          this.modelObservation = "From the visualization result we observe that the R squared score for the MLR model is above 95% when predicting the temperature and humidity, but less than 70% when predicting precipitation. The R squared score of 90% can be interpreted as 90% of the variations in temperature or humidity can be explained by changes in the other independent variables. Similarly, less than 70% of the variations in precipitation can be explained by the changes in other independent variables. Also, this can be seen by looking at the visualization graph, for both temperature and humidity, the points are close to the line of best fit, this is because the difference between the predicted and value for temperature and humidity are less than one, but higher than that.";
+          this.modelAnalysis= "MLR performs well in the weather dataset because the dataset meets the criteria/assumptions associated with a linear regression model which are: Linearity (the dependent and independent variables have a linear relationship), homoscedasticity (the variance of residual is the same for any value of the independent variable), independence (the observations are independent of each other), and normality (any fixed value of the features is normally distributed).";
       }
       else if (f.value["mlModels"][0] == "S"){
         modelAbbreviation = "SVR";
+          this.modelDescription = "Support vector regression used a hyper-plane to fit data and maintains two boundary lines on either side. Data points that fall on the hyperplane or within the boundary lines are the support vectors. The boundary lines are drawn at epsilon from the hyperplane.";
+          this.modelObservation = "From the visualization result, the R squared for the prediction result of random forest is above 90% for temperature and humidity but less than 60% for precipitation. R squared score is a matrix that measures how much variation in the dependent variable can be explained by the variation in the independent variables.";
+          this.modelAnalysis= "Support vector regression performs well for our model prediction because unlike the other regression models that tries to minimize the error between the actual and predicted values, SVR (Support Vector Regression) tries to fit the line of best fit within a threshold which is defined by the hyperplane and boundary lines. Support vector regression is suitable for large datasets; this is one of the reasons why SVR seems to perform less than Multiple Linear regression and Random Forest for our model.";
       }
       else{
         modelAbbreviation = "RF"
+        this.modelDescription = "Random forest regression uses many decision trees that operate as an ensemble. It uses individual decision trees to create an uncorrelated forest of trees. The prediction of random forest is usually better than that of an individual tree in most cases";
+        this.modelObservation = "From the visualization result, the R squared for the prediction result of random forest is above 95% for temperature and humidity but about 60% for precipitation. R squared score is a matrix that measures how much variation in the dependent variable can be explained by the variation in the independent variables.";
+        this.modelAnalysis= "From the R squared score, more than 95% of the variation in temperature or humidity can be attributed to the variations in the independent variables in the data set. Similarly, about 60% of the variation in precipitation can be attributed to the variation in the independent variables.";
       }
 
      this.secondPerformancePath = "timeseriesModels/" + f.value["mlModels"] + "/performanceCharts/" + "CHES_" + modelAbbreviation + "_" + pathDataset + "_" + f.value["features"] + "_" + f.value["windowSize"];
